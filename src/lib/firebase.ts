8de9d397bf2494127e3c9d711b6cb18bddd817f8
Cloud Engineer <cloudengineer08@gmail.com>
2025-08-13 05:14:27 +0000

    what next step

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,12 +5,13 @@ import { getFirestore } from 'firebase/firestore';
 
 // TODO: Replace this with the new firebaseConfig object from the new project
 const firebaseConfig = {
-  "projectId": "REPLACE_WITH_YOUR_PROJECT_ID",
-  "appId": "REPLACE_WITH_YOUR_APP_ID",
-  "storageBucket": "REPLACE_WITH_YOUR_STORAGE_BUCKET",
-  "apiKey": "REPLACE_WITH_YOUR_API_KEY",
-  "authDomain": "REPLACE_WITH_YOUR_AUTH_DOMAIN",
-  "messagingSenderId": "REPLACE_WITH_YOUR_MESSAGING_SENDER_ID"
+  apiKey: "AIzaSyCNSftfKdHRhscdFb1lRVJezwGP1-1X4xwA",
+  authDomain: "orderchha-app.firebaseapp.com",
+  projectId: "orderchha-app",
+  storageBucket: "orderchha-app.firebaseapp.com",
+  messagingSenderId: "442757956859",
+  appId: "1:442757956859:web:54e6935d539b2607785c5e",
+  measurementId: "G-QR6P8WW2Q4"
 };
 
 // Initialize Firebase
