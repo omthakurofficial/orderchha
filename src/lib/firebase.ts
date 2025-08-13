e06ccf7863b62aa3246647b8c3881982aaecc0e9
Cloud Engineer <cloudengineer08@gmail.com>
2025-08-13 07:28:41 +0000

    FirebaseError: Firebase: Error (auth/api-key-not-valid.-please-pass-a-va

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,13 +4,13 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore, deleteDoc } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAz_1-i4V3bF9gBvj5b8q2a3n7Kk9xY_wU",
-  authDomain: "orderchha-app.firebaseapp.com",
-  projectId: "orderchha-app",
-  storageBucket: "orderchha-app.appspot.com",
-  messagingSenderId: "442757956859",
-  appId: "1:442757956859:web:54e6935d539b2607785c5e",
-  measurementId: "G-QR6P8WW2Q4"
+  apiKey: "AIzaSyD_kZlqX8o6b4o_j5e5r6g7h8i9j0k_l1m",
+  authDomain: "orderchha-ra62f.firebaseapp.com",
+  projectId: "orderchha-ra62f",
+  storageBucket: "orderchha-ra62f.appspot.com",
+  messagingSenderId: "712345678901",
+  appId: "1:712345678901:web:1a2b3c4d5e6f7g8h9i0j",
+  measurementId: "G-ABCDEFGHIJ"
 };
 
 
