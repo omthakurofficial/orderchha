d11dec21c5c0b05a719a93c9ab79d7102b3d615a
Cloud Engineer <cloudengineer08@gmail.com>
2025-08-13 07:21:47 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,10 +4,10 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore, deleteDoc } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCNSftfKdHRhscdFb1lRVJezwGP1-1X4xwA",
+  apiKey: "AIzaSyAz_1-i4V3bF9gBvj5b8q2a3n7Kk9xY_wU",
   authDomain: "orderchha-app.firebaseapp.com",
   projectId: "orderchha-app",
-  storageBucket: "orderchha-app.firebaseapp.com",
+  storageBucket: "orderchha-app.appspot.com",
   messagingSenderId: "442757956859",
   appId: "1:442757956859:web:54e6935d539b2607785c5e",
   measurementId: "G-QR6P8WW2Q4"
